c3f3073122f42c357d0f9e2dda77de7fef109fde
KABAN <katrehko@bk.ru>
2023-09-15 18:59:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lr2/SecondActivity.java
@@ -0,0 +1,32 @@
+package com.example.lr2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(26);
+        textView.setPadding(16, 16, 16, 16);
+
+        Bundle arguments = getIntent().getExtras();
+
+        if(arguments!=null){
+            String name = arguments.get("name").toString();
+            String company = arguments.getString("company");
+            int age = arguments.getInt("age");
+            textView.setText("Name- " + name + "\nCompan- " + company +
+                    "\nAge- " + age);
+        }
+
+        setContentView(textView);
+    }
+
+}
